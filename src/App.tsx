72ad010c084a55d15215f5c12baea64b11015066
acer <acer@example.com>
2026-06-29 11:57:29 +0530

    Fix syntax error

--- src/App.tsx
+++ src/App.tsx
@@ -1631,11 +1631,10 @@ function VisitorPanel({ products, settings, setProducts }: { products: Product[]
                        {isSubmittingReview ? 'Submitting...' : 'Submit Review'}
                      </button>
                    </form>
-                 </div>
-               </div>
-            </div>
-            
-            {/* Action Bar */}
+                  </div>
+                </div>
+             
+             {/* Action Bar */}
             <div className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 p-3 pb-safe flex gap-3 shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.05)]">
                <button 
                  onClick={() => {
